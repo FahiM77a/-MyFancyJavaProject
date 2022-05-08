3e85e450abd42064ce78377dbc9e55c656ee643b
FahiM77a <muhammadfahimahmad1077@gmail.com>
2022-05-08 23:58:27 +0600

    Uploaded Rectangle

---
+++ src/rectangle/Rectangle.java
@@ -0,0 +1,14 @@
+
+package rectangle;
+
+public class Rectangle {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int width=5;  
+        int height=10;  
+        int area=width*height;  
+        System.out.println("Area of rectangle="+area);
+    }
+    
+}
