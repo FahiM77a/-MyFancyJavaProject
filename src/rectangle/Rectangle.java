48035a2ff6d306df57b1d82fadfd8a798adb49b3
Azmanur <arahman181196@bscse.uiu.ac.bd>
2022-05-10 22:38:37 +0600

    Code modified with Factory pattern

--- src/rectangle/Rectangle.java
+++ src/rectangle/Rectangle.java
@@ -1,14 +1,71 @@
+package creational.pattern;
 
-package rectangle;
+interface Shape
+{
+	void draw();
+}
 
-public class Rectangle {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
-        int width=5;  
-        int height=10;  
-        int area=width*height;  
-        System.out.println("Area of rectangle="+area);
-    }
-    
+class Circle implements Shape
+{
+	 @Override
+	 public void draw()
+	 {
+		 System.out.println("a circle drawn");
+	 }
+}
+
+
+class Square implements Shape
+{
+	 @Override
+	 public void draw()
+	 {
+		 System.out.println("a square drawn");
+	 }
+}
+
+class Rectangle implements Shape
+{
+	 @Override
+	 public void draw()
+	 {
+		 System.out.println("a rectangle drawn");
+	 }
+}
+
+
+class ShapeFactory
+{
+ 
+	 public Shape getShape(String type) throws Exception
+	 {
+		 switch (type)
+		 {
+			 case "Circle":
+				 return new Circle();
+			 case "Square":
+				 return new Square();
+			 case "Rectangle":
+				 return new Rectangle();
+			 default:
+				 throw new Exception( "Shape type : "+type+" cannot be instantiated");
+		 }
+	 }
+}
+
+class Painter
+{
+	 public static void main(String[] args) throws Exception
+	 {
+		 
+		 ShapeFactory shapeFactory = new ShapeFactory();
+		 Shape circle=shapeFactory.getShape("Circle");
+		 circle.draw();
+		 Shape square = shapeFactory.getShape("Square");
+		 square.draw();
+		 
+		 Shape rombus = shapeFactory.getShape("Rombus");
+		 rombus.draw();
+	 }
 }
